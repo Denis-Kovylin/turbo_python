bc75fdd92b7807ca3b60e2cd6ce3ede76245e67e
Denis-Kovylin <kovylin8519@gmail.com>
2026-03-31 13:04:53 +0300

    task with list of usernames was complited and added to file01_user_names_list

---
+++ 03_introduction_to_the_lists/01_access_to_list_items/01_user_names_list.py
@@ -0,0 +1,12 @@
+# Создан список имен юзеров. Обращаясь по идексу будут выведены все имена списка по очереди
+
+usernames = ['Adolf Hitler','Hermann Göring','Joseph Goebbels','Rudolf Hess','Heinrich Himmler','Wilhelm Keitel']
+
+print(len(usernames))
+
+print(usernames[0])
+print(usernames[1])
+print(usernames[2])
+print(usernames[3])
+print(usernames[4])
+print(usernames[5])
