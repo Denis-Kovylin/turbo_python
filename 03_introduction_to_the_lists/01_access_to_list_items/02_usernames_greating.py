da0e9e1ca42e4757ef334215482df07955f038d1
Denis-Kovylin <kovylin8519@gmail.com>
2026-03-31 13:19:32 +0300

    task with list of usernames and message for them  was complited. Also task  was added to file 02_usernames_greating.py

---
+++ 03_introduction_to_the_lists/01_access_to_list_items/02_usernames_greating.py
@@ -0,0 +1,16 @@
+# Создан список имен юзеров и меседж;
+# Обращаясь по идексу будут выведены все имена списка по очереди и сообщения для них
+
+usernames = ['Adolf Hitler','Hermann Göring','Joseph Goebbels','Rudolf Hess','Heinrich Himmler','Wilhelm Keitel']
+message = ('A true Aryan. He has a Nordic, stoic temperament. He performs his duties flawlessly.'
+           ' He is merciless toward the enemies of the Reich.'
+           ' Single: he has not been observed in any relationships that would discredit him.')
+
+print(len(usernames))
+
+print(f'{usernames[0]} - {message}')
+print(f'{usernames[1]} - {message}')
+print(f'{usernames[2]} - {message}')
+print(f'{usernames[3]} - {message}')
+print(f'{usernames[4]} - {message}')
+print(f'{usernames[5]} - {message}')
