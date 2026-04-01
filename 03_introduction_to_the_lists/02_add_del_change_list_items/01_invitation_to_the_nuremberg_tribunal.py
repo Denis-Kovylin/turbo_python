cb9d4e1d263746c40daf83f1f77aa09071c4db92
Denis-Kovylin <kovylin8519@gmail.com>
2026-04-01 11:09:16 +0300

    Task with invitation each user from list to tribunal was done and added to file 01_invitation_to_the_nuremberg_tribunal.py

---
+++ 03_introduction_to_the_lists/02_add_del_change_list_items/01_invitation_to_the_nuremberg_tribunal.py
@@ -0,0 +1,17 @@
+# Создан список людей подлежаших Нюрбернгскому трибуналу;
+# Создана повестка на явку в зал суда;
+# Поочередно каждому из них бутед "вручена повестка" методом переменных и обращения к списку по индексу;
+
+usernames = ['Adolf Hitler','Hermann Göring','Joseph Goebbels','Rudolf Hess','Heinrich Himmler','Wilhelm Keitel']
+gap = '\n'
+message = ('you are charged with crimes against humanity. '
+           'You are required to appear in the Nuremberg courtroom on November 20, 1945, for sentencing.')
+
+print(len(usernames))
+
+print(f'{usernames[0]}, {message}{gap}')
+print(f'{usernames[1]}, {message}{gap}')
+print(f'{usernames[2]}, {message}{gap}')
+print(f'{usernames[3]}, {message}{gap}')
+print(f'{usernames[4]}, {message}{gap}')
+print(f'{usernames[5]}, {message}{gap}')
