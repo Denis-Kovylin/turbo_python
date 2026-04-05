659efb26aa68274b0fd719eaf2544cd806f40a9a
Denis-Kovylin <kovylin8519@gmail.com>
2026-04-05 12:44:56 +0300

    File 02_if_elif_else_constructions.py was updated

--- 05_conditional_expressions/02_if_elif_else_constructions.py
+++ 05_conditional_expressions/02_if_elif_else_constructions.py
@@ -6,19 +6,19 @@ score_10_mess = 'Congratulations on taking out that monster! You get 10 points f
 score_15_mess = 'Congratulations on taking out that monster! You get 15 points for that.'
 error_mess = 'Something went wrong(( Please try again later!'
 alien_colors_tuple = ('green', 'yellow', 'red', 'blue')
-alien_color = alien_colors_tuple[3]
+alien_color = alien_colors_tuple[2]
 
 if alien_color == 'green':
-    print(score_5_mess)
+    print(score_5_mess, gap*3)
 elif alien_color == 'yellow':
-    print(score_10_mess)
+    print(score_10_mess, gap*3)
 elif alien_color == 'red':
-    print(score_15_mess)
+    print(score_15_mess, gap*3)
 else:
     print(error_mess, gap*3)
 
 '''пишем проверку на возрастную категорию'''
-age = 1000
+age = 10
 
 if age < 2:
     print('This is a baby')
@@ -34,6 +34,23 @@ elif age >= 65 and age <= 150:
     print('This is lod mam')
 else:
     print(error_mess)
+print(gap*3)
+
+'''Проверка на наличие фрукта в списке'''
+fruits: list = ['apples', 'bananas', 'mango', 'plums', 'watermelons']
+
+if 'apples' in fruits:
+    print(f'I really like {fruits[0]}')
+if 'bananas' in fruits:
+    print(f'I really like {fruits[1]}')
+if 'mango' in fruits:
+    print(f'I really like {fruits[2]}')
+if 'plums' in fruits:
+    print(f'I really like {fruits[3]}')
+if 'watermelons' in fruits:
+    print(f'I really like {fruits[4]}')
+
+
 
 
 
