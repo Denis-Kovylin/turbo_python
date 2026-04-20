ef10414dd5c13f6a215d64bbf372c03e3ae75bac
Denis-Kovylin <kovylin8519@gmail.com>
2026-04-20 20:42:04 +0300

    The task of processing user input has been completed. In addition, another documentation file has been added covering how to work with comments and the debugger. Files 01_user_input.py & dev_notes.md

---
+++ 07_while_loop_user_input/01_user_input.py
@@ -0,0 +1,34 @@
+'''Написать прогу, которая спрашивает какую машину хочет орендавать юзер. Вывести сообщение про
+желаемый автомобиль'''
+# auto_input = input('What kind of car do you want to rent? ')
+# print(f'Are you sure you want to rent {auto_input}')
+
+'''Написать прогу которая спрашивает юзера на сколько человек он хочет заказать стол в ресторане.
+Если число человек <= 8 вывести сообщение, что столиз забронирован. Если >8 - сообщеть
+что придеться подождать'''
+# reserve_table = input('How many people would you like to reserve a table for? ')
+# if not reserve_table.isdigit():
+#     print('Wrong input data')
+# else:
+#     reserve_table = int(reserve_table)
+#     if reserve_table < 1:
+#         print('The input must be a positive integer')
+#     elif reserve_table >= 1 and reserve_table <= 8:
+#         print(f'The table for {reserve_table} is reserved')
+#     else:
+#         print("You'll have to wait")
+
+'''Попросить пользователя ввести число. Вывести результат обработки: число кратно 10ти или нет'''
+# HACK Адекватне проверки через try/except делать
+# num_input = input('Enter number: ')
+# if not num_input[1:].isdigit():
+#     print('Wrong data type was entered!')
+# else:
+#     num_input = int(num_input)
+#     if num_input < 0:
+#         print('Number must be positive!')
+#     elif num_input % 10 == 0:
+#         print('a multiple of 10')
+#     else:
+#         print('not a multiple of 10.')
+
