b7c43ed0a757fb90a34870ad9eb351f19d97eb6c
Denis-Kovylin <kovylin8519@gmail.com>
2026-04-26 09:18:27 +0300

    basic functions exercises with function was done. file 01_basic_functions.py

---
+++ 08_functions/01_basic_functions.py
@@ -0,0 +1,18 @@
+'''8-1 Сообщение
+Создать функцию, которая будет выводить сообщение об этом разделе
+'''
+# def info():
+#     '''this chapter information'''
+#     print('In Python, a function is a self-contained block of code designed to perform a specific task. '
+#           'They are essential for breaking down complex problems into smaller, manageable parts—a design '
+#           'principle known as modularity. ')
+# info()
+
+'''8-2 Любимая книга
+Напсиать функцию, которая будет принимать один параметр ( название книги ) и будет выводить сообщение
+про эту книгу
+'''
+# def favorite_book(title):
+#     '''favorite book name'''
+#     print(f'My favorite book is {title}')
+# favorite_book('1984')
