a8a3cae9f4100850f89b3b25a6d8359beb8b8c65
Denis-Kovylin <kovylin8519@gmail.com>
2026-04-26 09:47:49 +0300

    exercises with deferent types of fanction arguments was done. File 02_function_arguments.py

---
+++ 08_functions/02_function_arguments.py
@@ -0,0 +1,28 @@
+'''8-3. Футболка
+Написать функцию make_shirt() с двумя параметрами — размер и надпись.
+Вызвать дважды: первый раз позиционными аргументами, второй раз ключевыми.'''
+# def make_shirt(size, text):
+#     print(f'size: {size.title()}; slogan: {text.upper()}')
+# make_shirt('l', 'just do it')
+# make_shirt(text='Anima sana in corpore sano', size='xl')
+
+'''8-4. Футболки L
+Добавить дефолтные значения к параметрам — размер L и текст I love Python. 
+Вызвать три раза: два с дефолтами (L и M), один с другим текстом.
+'''
+# def make_shirt(size='L', text='I love python'):
+#     print(f'size: {size}; text: {text}')
+# make_shirt()
+# make_shirt(size='XL')
+# make_shirt(text='JS === shit')
+
+'''8-5. Міста
+Функция describe_city() — два параметра: город и страна. Страна имеет дефолтное значение. 
+Вызвать три раза с разными городами.
+'''
+# def describe_city(city, country='USA'):
+#     print(f'{city.title()} is in {country}')
+# describe_city('New York')
+# describe_city('Dalas')
+# describe_city('Tokio', 'Japan')
+
