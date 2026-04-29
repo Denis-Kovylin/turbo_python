740ff80ed453a5cae4a48b5896f5ccc6f77a9646
Denis-Kovylin <kovylin8519@gmail.com>
2026-04-29 21:16:08 +0300

    exercise abuon functions with lists was done. File: 04_function_with_lists.py

---
+++ 08_functions/04_function_with_lists.py
@@ -0,0 +1,42 @@
+'''8-9. Сообщения
+Передать список сообщений в функцию show_messages() — она просто выводит каждое.'''
+# messages = ['hey what up', 'yo its a me mario', 'python is cool', 'send help']
+# def show_messages(mess_list):
+#     for mess in mess_list:
+#         print(mess)
+# show_messages(messages)
+
+'''8-10. Отправка сообщений
+Функция send_messages() — выводит каждое сообщение, перемещает его из messages в sent_messages. 
+После вызова вывести оба списка.'''
+# messages = ['hey what up', 'yo its a me mario', 'python is cool', 'send help']
+# sent_messages = []
+# def send_messages(mess_list,snt_mess_list):
+#     while mess_list:
+#         current_mess = mess_list.pop()
+#         print(f'Sending message: {current_mess}')
+#         snt_mess_list.append(current_mess)
+#     print('\nMessages')
+#     print(mess_list)
+#     print('\nSend messages: ')
+#     for mess in snt_mess_list:
+#         print(f'\t{mess}')
+# send_messages(messages, sent_messages)
+
+'''8-11. Заархивированные сообщения
+То же что 8-10, но передаёшь функции копию списка messages[:]. 
+После вызова оригинальный список должен остаться нетронутым.'''
+# messages = ['hey what up', 'yo its a me mario', 'python is cool', 'send help']
+# sent_messages = []
+# def archive_messages(mess_list, origin_list, snt_list):
+#     while mess_list:
+#         current_mess = mess_list.pop()
+#         print(f'Sending message: {current_mess}')
+#         snt_list.append(current_mess)
+#     print('\nMessages:')
+#     for mess in origin_list:
+#         print(f'\t{mess}')
+#     print('\nSent messages:')
+#     for sent_mess in snt_list:
+#         print(f'\t{sent_mess}')
+# archive_messages(messages[:], messages, sent_messages)
